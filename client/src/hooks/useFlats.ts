110351930fc2409fe328b627abd660cafcab1b49
Gregor Hočevar <hocevar.gregi@gmail.com>
2022-10-18 17:34:46 +0200

    useFlats to use axios and follow rules of hooks

--- client/src/hooks/useFlats.ts
+++ client/src/hooks/useFlats.ts
@@ -1,20 +1,21 @@
 import useSWR from 'swr';
+import axios from 'axios';
 import { IFlatsResponse } from '../interfaces/IFlat';
 
-const fetcher = (url: string) => fetch(url).then(res => res.json());
+const fetcher = async (url: string) => {
+  try {
+    const { data } = await axios.get(url);
+    return data;
+  } catch (err) {
+    // @ts-ignore
+    throw new Error('Error: ', err);
+  }
+};
 
+const take = 20;
 const url = 'http://localhost:3000/api/v1/flats';
 
 export const useFlats = (page: number = 1) => {
-  if (page < 1) {
-    return {
-      data: null,
-      isLoading: false,
-      error: new Error('Invalid page'),
-    };
-  }
-
-  const take = 20;
   const { data, error } = useSWR<IFlatsResponse>(
     `${url}?take=${take}&skip=${(page - 1) * take}`,
     fetcher
