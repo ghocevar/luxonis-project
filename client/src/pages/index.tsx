a550c8b779acc76caee4a64505b153aaafc672a7
Gregor Hočevar <hocevar.gregi@gmail.com>
2022-10-18 13:31:45 +0200

    Increase title size & add missing sharp lib

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,9 +2,7 @@ import FlatsContainer from '../containers/FlatsContainer';
 
 const Home = () => (
   <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-    <h1 className="pb-10 font-bold text-2xl">
-      Aparments list from sreality.cz
-    </h1>
+    <h1 className="pb-10 font-bold text-3xl">Aparment list from sreality.cz</h1>
     <FlatsContainer />
   </div>
 );
